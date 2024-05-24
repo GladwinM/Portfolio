a4676805655a0863ca738ed78b801c72c4bf4c1f
GladwinM <gladwinjeuish@gmail.com>
2024-05-24 11:48:46 +0530

    fgh

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -16,7 +16,7 @@ export const ThemeProvider = ({ children }) => {
     </ThemeContext.Provider>
   );
 };
-
+//done
 export const useTheme = () => useContext(ThemeContext);
 
 ThemeProvider.propTypes = {
